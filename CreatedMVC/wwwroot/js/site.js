a4bb8d1903c958019ef097f8ac9f31624123fcd4
victtorH <estevamvicttor@gmail.com>
2025-09-17 03:02:45 -0300

    Limpando js

--- CreatedMVC/wwwroot/js/site.js
+++ CreatedMVC/wwwroot/js/site.js
@@ -1,4 +1,2 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 
-var meuAlerta = $("#meuAlerta").attr("meuAlerta")
-console.log(meuAlerta)
